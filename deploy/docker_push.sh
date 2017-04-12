dadc8b30985c1333507a8864f2acbb7afd6e6b1d
markTward <mark.t.ward@gmail.com>
2017-04-11 17:24:43 -0700

    docker push all builds using abbreviated commit sha

--- deploy/docker_push.sh
+++ deploy/docker_push.sh
@@ -2,26 +2,25 @@
 
 echo "docker_push.sh script start"
 
-env | grep TRAVIS | sort
 docker version
 
-DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]')
+echo "docker repo: $DOCKER_REPO"
+echo "docker commit tag: $DOCKER_COMMIT_TAG"
+echo "docker build/push for TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
 
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
-  echo "docker build/push for TRAVIS_EVENT_TYPE=pull_request"
-  docker tag $DOCKER_REPO:$TRAVIS_COMMIT $DOCKER_REPO:PR-$TRAVIS_PULL_REQUEST;
-  docker images
-  docker push $DOCKER_REPO:PR-$TRAVIS_PULL_REQUEST
+  docker tag $DOCKER_REPO:$DOCKER_COMMIT_TAG $DOCKER_REPO:PR-$TRAVIS_PULL_REQUEST;
 fi
 
 if [ "$TRAVIS_EVENT_TYPE" == "push" ]; then
   echo "docker build/push for TRAVIS_EVENT_TYPE=push"
-  docker tag $DOCKER_REPO:$TRAVIS_COMMIT $DOCKER_REPO:$TRAVIS_BRANCH;
+  docker tag $DOCKER_REPO:$DOCKER_COMMIT_TAG $DOCKER_REPO:$TRAVIS_BRANCH;
   if [ "$TRAVIS_BRANCH" == "master"]; then
-    docker tag $DOCKER_REPO:$TRAVIS_COMMIT $DOCKER_REPO:latest;
+    docker tag $DOCKER_REPO:$DOCKER_COMMIT_TAG $DOCKER_REPO:latest;
   fi
-  docker images
-  docker push $DOCKER_REPO:$TRAVIS_BRANCH
 fi
+
+docker images
+docker push $DOCKER_REPO
