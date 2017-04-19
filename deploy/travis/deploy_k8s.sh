9512ca919730f9e43678f08e1e56d16dafab2b4b
markTward <mark.t.ward@gmail.com>
2017-04-19 14:50:09 -0700

    after reorg deployment debug

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 echo "helm install start"
-sudo helm list --all
 sudo helm upgrade --dry-run --debug --install gocloud --namespace=gocloud --set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG --set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG deploy/helm/gocloud/
-sudo helm list --all
-
-sudo kubectl get svc,deploy,pod --namespace=gocloud
