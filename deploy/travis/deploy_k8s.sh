563042cb046012784318d558d5fb20d7df1007c3
markTward <mark.t.ward@gmail.com>
2017-04-20 11:39:51 -0700

    begin parameterizing k8s deployment script

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -1,29 +1,45 @@
 #!/usr/bin/env bash
 # TODO: make namespace and template dir args
 
-echo "helm install start"
+echo "helm/k8s install"
 echo "args: $@"
 
+# TODO: move to travis config and use as arg to script
 if [[ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]];
 then export DOCKER_REPO=gcr.io/GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME;
 else export DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]');
 fi
 
-if [[ $# -eq 2 ]] && [ $2 == "DRYRUN" ];
+if [[ $# -ne 3 ]]
+then
+  echo "error: incorrect number of required positional args"
+  echo "usage: deploy_k8s.sh deploy-type dry-run namespace"
+  exit 1
+fi
+
+DEPLOY_TYPE=$1
+DRYRUN=$2
+NAMESPACE=$3
+
+if [ $DRYRUN == "DRYRUN" ];
   then
     DRYRUN_OPTION=" --dry-run "
     echo "using --dry-run option; service not deployed."
 fi
 
-echo project: $GOCLOUD_PROJECT_NAME
-echo image: $DOCKER_REPO:$DOCKER_COMMIT_TAG
+echo deployment type: $DEPLOY_TYPE
 echo dryrun: $DRYRUN_OPTION
+echo namespace: $NAMESPACE
+echo project: $GOCLOUD_PROJECT_NAME
+echo branch: $TRAVIS_BRANCH
+echo image: $DOCKER_REPO:$DOCKER_COMMIT_TAG
 
-sudo helm upgrade \
+# sudo helm upgrade \
+helm upgrade \
 $DRYRUN_OPTION \
 --debug \
 --install $GOCLOUD_PROJECT_NAME \
---namespace=gocloud \
+--namespace=$NAMESPACE \
 --set service.gocloudAPI.image.repository=$DOCKER_REPO \
 --set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG \
 --set service.gocloudGrpc.image.repository=$DOCKER_REPO \
