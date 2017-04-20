6dcecc074f1279ba8fe975d12c2e1cad726c28c6
markTward <mark.t.ward@gmail.com>
2017-04-19 19:14:50 -0700

    problems setting repo in travis file. returning/duplicating to scripts for now.

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -2,8 +2,19 @@
 echo "helm install start"
 echo "args: $@"
 
+if [[ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]];
+then export DOCKER_REPO=gcr.io/GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME;
+else export DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]');
+fi
+
+if [[ $# -eq 2 ]] && [ $2 == "DRYRUN" ];
+  then
+    DRYRUN_OPTION=" --dry-run "
+    echo "using --dry-run option; service not deployed."
+fi
+
 sudo helm upgrade \
---dry-run --debug \
+-- debug --$DRYRUN_OPTION \
 --install $GOCLOUD_PROJECT_NAME \
 --namespace=gocloud \
 --set service.gocloudAPI.image.repository=$DOCKER_REPO \
