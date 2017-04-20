4c87704568a5e716393246de4c1a8a094507587e
markTward <mark.t.ward@gmail.com>
2017-04-19 18:32:23 -0700

    attempt conditional branch in deploy step; move docker repo var upstream for reuse

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 echo "helm install start"
+echo "args: $@"
+
 sudo helm upgrade --dry-run --debug --install gocloud --namespace=gocloud \
+--set service.gocloudAPI.image.repository=$DOCKER_REPO \
 --set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG \
---set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG deploy/helm/gocloud
+--set service.gocloudGrpc.image.repository=$DOCKER_REPO \
+--set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG \
+deploy/helm/gocloud
