4c34480155315932372993e9440a403da86732d5
markTward <mark.t.ward@gmail.com>
2017-04-19 20:14:56 -0700

    travis file debugging

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -19,12 +19,11 @@ echo project: $GOCLOUD_PROJECT_NAME
 echo image: $DOCKER_REPO:$DOCKER_COMMIT_TAG
 echo dryrun: $DRYRUN_OPTION
 
-sudo helm upgrade \
+sudo helm upgrade deploy/helm/gocloud \
 --debug --$DRYRUN_OPTION \
 --install $GOCLOUD_PROJECT_NAME \
 --namespace=gocloud \
 --set service.gocloudAPI.image.repository=$DOCKER_REPO \
 --set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG \
 --set service.gocloudGrpc.image.repository=$DOCKER_REPO \
---set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG \
-deploy/helm/gocloud
+--set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG
