fa28ac495ee76627751fd87bea6add53d27253fa
markTward <mark.t.ward@gmail.com>
2017-04-19 18:07:39 -0700

    move google container registry handling to gocloud-cicd script

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 echo "helm install start"
-sudo helm upgrade --dry-run --debug --install gocloud --namespace=gocloud --set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG --set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG deploy/helm/gocloud/
+sudo helm upgrade --dry-run --debug --install gocloud --namespace=gocloud \
+--set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG \
+--set service.gocloudGrpc.image.tag=$DOCKER_COMMIT_TAG deploy/helm/gocloud
