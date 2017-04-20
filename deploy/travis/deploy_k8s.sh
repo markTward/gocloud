5b125e74ddf1fe71d91419e36e071a10bf72ab1d
markTward <mark.t.ward@gmail.com>
2017-04-19 18:44:23 -0700

    move conditional logic for selecting repo into build config for reuse among scripts

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -2,7 +2,10 @@
 echo "helm install start"
 echo "args: $@"
 
-sudo helm upgrade --dry-run --debug --install gocloud --namespace=gocloud \
+sudo helm upgrade \
+--dry-run --debug \
+--install $GOCLOUD_PROJECT_NAME \
+--namespace=gocloud \
 --set service.gocloudAPI.image.repository=$DOCKER_REPO \
 --set service.gocloudAPI.image.tag=$DOCKER_COMMIT_TAG \
 --set service.gocloudGrpc.image.repository=$DOCKER_REPO \
