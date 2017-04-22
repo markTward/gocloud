0185d9eaf0939003421cb5faaa16e447f63b8365
markTward <mark.t.ward@gmail.com>
2017-04-22 15:09:02 -0700

    add branch regex login for determining repo target

--- deploy/travis/docker_push.sh
+++ deploy/travis/docker_push.sh
@@ -3,23 +3,20 @@
 echo "docker_push.sh script start"
 docker version
 
-# if [[ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]];
-# then
-#   DOCKER_REPO=gcr.io/$GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME;
-#   REPO_TARGET=gcr
-# else
-#   DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]');
-#   REPO_TARGET=docker
-# fi
-
-DOCKER_REPO=gcr.io/$GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME
+if [[ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]];
+then
+  DOCKER_REPO=gcr.io/$GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME;
+  REPO_TARGET=gcr
+else
+  DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]');
+  REPO_TARGET=docker
+fi
 
 echo "DOCKER_REPO: $DOCKER_REPO"
+echo "REPO_TARGET: $REPO_TARGET"
 echo "DOCKER_COMMIT_TAG: $DOCKER_COMMIT_TAG"
-echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
 echo "BRANCH_REGEX: $BRANCH_REGEX"
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "TRAVIS_EVENT_TYPE: $TRAVIS_EVENT_TYPE"
 
 # tag images
 docker tag $GOCLOUD_PROJECT_NAME:$DOCKER_COMMIT_TAG $DOCKER_REPO:$DOCKER_COMMIT_TAG;
@@ -29,13 +26,14 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 fi
 docker images
 
-if [ $REPO_TARGET == "gcr"]; then
+# push images
+if [ "$REPO_TARGET" == "gcr" ]; then
   sudo gcloud docker -- push $DOCKER_REPO:$DOCKER_COMMIT_TAG
   sudo gcloud docker -- push $DOCKER_REPO:$TRAVIS_BRANCH
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     sudo gcloud docker -- push $DOCKER_REPO:latest
   fi
-elif [ $REPO_TARGET == "docker"]; then
+elif [ "$REPO_TARGET" == "docker" ]; then
   docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
   docker push $DOCKER_REPO
 else
