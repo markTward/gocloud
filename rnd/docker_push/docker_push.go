2bc7896c3a2426e2f81b8f63773e7949699d1ecf
markTward <mark.t.ward@gmail.com>
2017-04-27 21:43:50 -0700

    set stage for calling subscripts for gcr and docker registry push

--- rnd/docker_push/docker_push.go
+++ rnd/docker_push/docker_push.go
@@ -1,11 +1,15 @@
 package main
 
+// TODO: commit tag, event type and branch flags and logic
+// TODO: create one each subscripts for gcr and docker
+
 import (
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -15,16 +19,29 @@ type Config struct {
 }
 
 const (
-	defaultConfigFile = "cicd.yaml"
+	defaultConfigFile  = "cicd.yaml"
+	defaultRegistryURL = ""
+	configFileUsage    = "configuration file containing project workflow values"
+	registryURLUsage   = "provide a valid registry URL (docker and gcr currenly supported)"
 )
+
+var configFile string
+var registryURL string
+
+func init() {
+
+	flag.StringVar(&configFile, "config", defaultConfigFile, configFileUsage)
+	flag.StringVar(&configFile, "c", defaultConfigFile, configFileUsage)
+
+	flag.StringVar(&registryURL, "url", defaultRegistryURL, registryURLUsage)
+	flag.StringVar(&registryURL, "u", defaultRegistryURL, registryURLUsage)
+}
 
 func main() {
 
-	// setup flags
-	configFile := flag.String("config", defaultConfigFile, "configuration file containing project workflow values")
 	flag.Parse()
 
-	yamlInput, err := ioutil.ReadFile(*configFile)
+	yamlInput, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
@@ -37,16 +54,25 @@ func main() {
 		log.Fatalf("error: %v", err)
 	}
 
-	fmt.Printf("Config.Registry ==> %v\n", cfg.Registry)
-
-	switch cfg.Registry.Name {
-	case "gcr":
-		fmt.Println("pushing to GCR!!!", cfg.Registry)
-	case "docker":
-		fmt.Println("pushing to DOCKER!!!", cfg.Registry)
-	default:
-		fmt.Println("unknown Registry :-()")
+	if registryURL != "" {
+		if !(strings.HasPrefix(registryURL, "gcr.io") || strings.HasPrefix(registryURL, "docker.io")) {
+			fmt.Fprintf(os.Stderr, "error: unsupported registry: %v\n", strings.Split(registryURL, "/")[0])
+			os.Exit(1)
+		}
+	} else {
+		switch cfg.Registry.Name {
+		case "gcr":
+			registryURL = fmt.Sprintf("%v/%v/%v", cfg.Registry.Host, cfg.Registry.Project, cfg.Registry.Repo)
+			// TODO: os.exec gcr_push.sh
+		case "docker":
+			registryURL = fmt.Sprintf("%v/%v/%v", cfg.Registry.Host, cfg.Registry.Account, cfg.Registry.Repo)
+			// TODL: os.exec docker_push.sh
+		default:
+			fmt.Fprintf(os.Stderr, "error: unsupported registry: %v\n", cfg.Registry.Name)
+			os.Exit(1)
+		}
 	}
+	fmt.Println("Registry URL:", registryURL)
 
 	// debugYAML(yamlInput, cfg)
 }
