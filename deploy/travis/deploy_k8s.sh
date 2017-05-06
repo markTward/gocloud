e98fc2c7b5c369f601e73db1f3a2c580d2af28da
markTward <mark.t.ward@gmail.com>
2017-05-05 17:41:27 -0700

    EOD cleanup [ci skip]

--- deploy/travis/deploy_k8s.sh
+++ deploy/travis/deploy_k8s.sh
@@ -4,11 +4,7 @@
 echo "helm/k8s install"
 echo "args: $@"
 
-# TODO: move to travis config and use as arg to script
-# if [[ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]];
-# then export DOCKER_REPO=gcr.io/$GCLOUD_PROJECT_ID/$GOCLOUD_PROJECT_NAME;
-# else export DOCKER_REPO=$(echo $TRAVIS_REPO_SLUG | tr '[:upper:]' '[:lower:]');
-# fi
+# TODO: derive DOCKER_REPO from cicd.yaml if move to go-based script
 
 if [[ $# -ne 5 ]]
 then
